53f75fdc9ce1da224a263dca8cdc5f9f5314318a
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-09-05 23:37:12 -0400

    first commit

---
+++ hw.py
@@ -0,0 +1,21 @@
+import re
+
+with open ('./user_records.txt') as f:
+    data = f.readlines()
+
+pattern = re.compile('\d{2}), ([A-Z][\w+]+)')
+
+true_count = 0
+false_count = 0
+
+for d in data:
+    found_num = pattern.search(d)
+    if found_num:
+        age = found_num.group(1)
+        countries = found_num.group(2)
+        print(f'Age: {age} \nCountry: {countries}\n')
+        true_count += 1
+    else:
+        false_count += 1
+        
+print(f'Valid Count: {true_count}\nInvalid Count: {false_count}')
